ebaefd3e394539d918bfbcb455b6a617e4999857
Rahul Katiyar <katiyar@Rahuls-MacBook-Air.local>
2024-09-14 11:14:51 +0530

    Refactored the written code.

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,23 +1,30 @@
 
 
 export function add(numbers: string): number {
-    if (numbers === "") {
-        return 0;
-    }
-    let sum: number = 0;
-    for (let num of numbers.split(",")) {
-        sum = sum + Number(num);
-    }
+
+    let sum:number = 0;
+    sum = numbers.split(",").reduce((accumulator, currentValue)=>{
+        return Number(accumulator) + Number(currentValue);
+    }, sum);
+
     return sum;
 }
 
-let result = add("1,2");
+let result = add("1,2,4,5");
 console.log(result);
 
 
 
 
+    // if (numbers === "") {
+    //     return 0;
+    // }
 
+    // let sum: number = 0;
+    // for (let num of numbers.split(",")) {
+    //     sum = sum + Number(num);
+    // }
+    // return sum;
 
 
 // function stringCalculator(...args: number[]) {
