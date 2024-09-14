fc4cbd8db88609490fb8ba215ecdbef820a838cb
Rahul Katiyar <katiyar@Rahuls-MacBook-Air.local>
2024-09-14 09:40:57 +0530

    first commit

---
+++ src/app/index.ts
@@ -0,0 +1,24 @@
+
+
+export function sum(a: number, b: number) {
+    return a + b;
+}
+
+let temp = sum(2,5);
+console.log(temp);
+
+
+
+
+
+
+
+// function stringCalculator(...args: number[]) {
+//     return args.reduce((accumulator, currentValue) => {
+//         return accumulator += currentValue;
+//     }, 0);
+// }
+
+// let sum = stringCalculator(1,2,3,4,5,6,7,8);
+
+
